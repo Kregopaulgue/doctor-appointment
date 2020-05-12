6892dc810d5d8657aad4270065638751bcd2f6ed
Paul <kregopaulgue@gmail.com>
2020-05-12 19:38:03 +0300

    Add times routes

---
+++ server/routes/time.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+
+const { TimeModel } = require('../models/Time.js');
+
+router.get('',
+    async (req, res) => {
+        try {
+            const times = await TimeModel.find({});
+            const payload = { times };
+
+            return res.status(200).json(payload);
+        } catch(error) {
+            console.log(error);
+            return res.status(500).send('Error while loading times');
+        }
+    }
+)
