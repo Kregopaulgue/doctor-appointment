be00ac2e49637306799c71af0de054bdb36dfbf6
Paul <kregopaulgue@gmail.com>
2020-05-14 15:20:27 +0300

    Develop component

--- client/src/components/appointments/AvailableAppointments.js
+++ client/src/components/appointments/AvailableAppointments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Table } from 'react-bootstrap';
+
 import { AppointmentsModel } from '../../services/api/models/appointments.js';
 import { UsersModel } from '../../services/api/models/users.js';
 import { TimesModel } from '../../services/api/models/times.js';
@@ -16,10 +18,10 @@ class AvailableAppointments extends Component {
             time: null,
 
             appointments: [],
-            filteredAppointments: [],
 
             days: {},
             dayTimes: {},
+            filteredDayTimes: {},
 
             times: []
         };
@@ -27,13 +29,15 @@ class AvailableAppointments extends Component {
     async componentDidMount() {
         await this.loadTimes();
         this.setWeekDays(Date.now());
+        await this.loadSearchAppointments();
+        this.filterDayTimes();
     }
     loadTimes = async () => {
         try {
             const timesModelInstance = new TimesModel();
             const response = await timesModelInstance.getAllTimes();
 
-            this.setState({ times: response.times });
+            this.setState({ times: response.times || [] });
         } catch(error) {
             console.log(error);
         }
@@ -49,16 +53,16 @@ class AvailableAppointments extends Component {
         const monday = new Date(currentDate.setDate(difference));
         const tuesday = new Date();
         tuesday.setDate(monday.getDate() + 1);
-        tuesday.setUTCHours(0, 0, 0);
+        tuesday.setUTCHours(0, 0, 0, 0);
         const wednesday = new Date();
         wednesday.setDate(tuesday.getDate() + 1);
-        wednesday.setUTCHours(0, 0, 0);
+        wednesday.setUTCHours(0, 0, 0, 0);
         const thursday = new Date();
         thursday.setDate(wednesday.getDate() + 1);
-        thursday.setUTCHours(0, 0, 0);
+        thursday.setUTCHours(0, 0, 0, 0);
         const friday = new Date();
         friday.setDate(thursday.getDate() + 1);
-        friday.setUTCHours(0, 0, 0);
+        friday.setUTCHours(0, 0, 0, 0);
         
         const days = {
             monday,
@@ -128,14 +132,85 @@ class AvailableAppointments extends Component {
 
         this.setState({ days, dayTimes });
     }
-    loadSearchAppointments = () => {
-
+    loadSearchAppointments = async () => {
+        try {
+            const appointmentsModelInstance = new AppointmentsModel();
+            const response = await appointmentsModelInstance.searchAppointments({
+                fromDate: this.state.days.monday.toISOString(),
+                toDate: this.state.days.friday.toISOString()
+            });
+            this.setState({ appointments: response.appointments || [] });
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    filterDayTimes = () => {
+        let filteredDayTimes = Object.assign({}, this.state.dayTimes);
+        const { appointments } = this.state;
+        filteredDayTimes.mondayTimes = filteredDayTimes.mondayTimes.filter((dayTime) => {
+            const sameDateAppointment = appointments.find(appoint => {
+                return (new Date(appoint.date)).getTime() === dayTime.getTime();
+            });
+            return !(!!sameDateAppointment); 
+        });
+        filteredDayTimes.tuesdayTimes = filteredDayTimes.tuesdayTimes.filter((dayTime) => {
+            const sameDateAppointment = appointments.find(appoint => {
+                return (new Date(appoint.date)).getTime() === dayTime.getTime();
+            });
+            return !(!!sameDateAppointment); 
+        });
+        filteredDayTimes.wednesdayTimes = filteredDayTimes.wednesdayTimes.filter((dayTime) => {
+            const sameDateAppointment = appointments.find(appoint => {
+                return (new Date(appoint.date)).getTime() === dayTime.getTime();
+            });
+            return !(!!sameDateAppointment); 
+        });
+        filteredDayTimes.thursdayTimes = filteredDayTimes.thursdayTimes.filter((dayTime) => {
+            const sameDateAppointment = appointments.find(appoint => {
+                return (new Date(appoint.date)).getTime() === dayTime.getTime();
+            });
+            return !(!!sameDateAppointment); 
+        });
     }
 
     render() {
         return (
             <div>
-                Test 2
+                {
+                    this.state.days.monday &&
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    <p>Monday</p>
+                                    <p>{this.state.days.monday.toISOString()}</p>
+                                </th>
+                                <th>
+                                    <p>Tuesday</p>
+                                    <p>{this.state.days.tuesday.toISOString()}</p>
+                                </th>
+                                <th>
+                                    <p>Wednesday</p>
+                                    <p>{this.state.days.wednesday.toISOString()}</p>
+                                </th>
+                                <th>
+                                    <p>Thursday</p>
+                                    <p>{this.state.days.thursday.toISOString()}</p>
+                                </th>
+                                <th>
+                                    <p>Friday</p>
+                                    <p>{this.state.days.friday.toISOString()}</p>
+                                </th>
+                            </tr>
+                        </thead>
+                        {
+                            this.state.appointments.length > 0 &&
+                            <tbody>
+
+                            </tbody>
+                        }
+                    </Table>
+                }
             </div>
         );
     }
