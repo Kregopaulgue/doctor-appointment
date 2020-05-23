24439351b7ac096bb3ce6d37c2593f1f4b644d67
Paul <kregopaulgue@gmail.com>
2020-05-23 23:49:13 +0300

    gefg

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+
 
 const usersRouter = require('./routes/users.js');
 const timesRouter = require('./routes/times.js');
@@ -9,27 +11,27 @@ const doctorsRotuer = require('./routes/doctors.js');
 
 const { initiateMongoServer } = require('./config/db.js');
 
-initiateMongoServer();
-
 const app = express();
 
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+
+initiateMongoServer();
 
 app.use('/api/users', usersRouter);
 app.use('/api/times', timesRouter);
 app.use('/api/doctors', doctorsRotuer);
 app.use('/api/appointments', appointmentsRouter);
 
-if(process.env.NODE_ENV === 'production') {
+if(true) {
     app.use(express.static(__dirname + '/public/'));
     app.get(/.*/, (req, res) => {
         res.sendFile(__dirname + '/public/index.html');
     });
 }
 
-const port = process.env.PORT || 5000;
-
+const port = 5000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server started on ${port}`);
 });
